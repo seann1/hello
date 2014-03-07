612f88c4aad460893d944926217dcc9145221070
Maciej Skierkowski <maciej@skierkowski.com>
2014-03-07 00:12:02 -0800

    adding rspec and cap support

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'sandbox'
+set :repo_url, 'git@github.com:skierkowski/hello.git'
+set :deploy_to, '/home/ubuntu/webtest'
+set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
