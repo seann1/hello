d24554675592207a3420a1397eb0ee3e89fb46b9
Maciej Skierkowski <maciej@skierkowski.com>
2014-03-26 12:49:11 -0700

    forward agent true, default settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.1.0'
 set :application, 'sandbox'
 set :repo_url, 'git@github.com:skierkowski/hello.git'
 set :deploy_to, '/home/ubuntu/webtest'
-# set :ssh_options, {:forward_agent=>true, :keys=>['/repo_key.pem']}
+set :ssh_options, {:forward_agent=>true}
 set :scm, :git
 set :format, :pretty
 set :log_level, :debug
